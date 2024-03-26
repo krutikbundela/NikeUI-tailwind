38821d3be2ffaf7085874c555918ead3b754baf2
Krutik Bundela <krutikbundela2001@gmail.com>
2024-03-26 11:43:05 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// https://gist.github.com/adrianhajdin/ae10e2dd6ee5d51fc5b31437f73cf014
+// https://drive.google.com/file/d/1ccqjc8gJ7CLvXT_vUhVT4Gmys-Ze13FK/view
+// https://youtu.be/tS7upsfuxmo
---
+++ src/sections/index.js
@@ -0,0 +1,11 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+
+export {Hero , PopularProduct, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer,}
